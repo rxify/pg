41adda4b8d48865af4597a3c96bb260eb3331a28
Alexander Porrello <alexander.porrello@kapsch.net>
2024-06-12 22:03:27 -0500

    Updates dependencies.

--- tools/postbuild.js
+++ tools/postbuild.js
@@ -1,9 +1,13 @@
+import { execSync } from 'child_process';
 import { cpSync, readFileSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 
 const conf = JSON.parse(readFileSync(resolve('package.json'), 'utf-8'));
 delete conf.devDependencies;
 delete conf.scripts;
+delete conf.workspaces;
+conf.bin['pg-runner'] = 'cli/bin.js';
 
 writeFileSync(resolve('dist/package.json'), JSON.stringify(conf, null, 4));
 cpSync(resolve('README.md'), resolve('dist', 'README.md'));
+execSync('chmod 755 dist/cli/bin.js');
